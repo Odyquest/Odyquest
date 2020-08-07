928417a5653b069acb9347333299584aa90edab1
florian goebel <florian.goebel@outlook.de>
2020-08-07 07:56:21 +0200

    Added actuator to ms_quest and ms_config.

--- ms_quest/build.gradle.kts
+++ ms_quest/build.gradle.kts
@@ -20,17 +20,18 @@ repositories {
 extra["springCloudVersion"] = "2020.0.0-SNAPSHOT"
 
 dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-actuator")
 	implementation("org.springframework.boot:spring-boot-starter-data-mongodb-reactive")
 	implementation("org.springframework.boot:spring-boot-starter-hateoas")
 	implementation("org.springframework.boot:spring-boot-starter-security")
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("org.springframework.boot:spring-boot-starter-webflux")
+	implementation("org.springframework.cloud:spring-cloud-starter-zipkin")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("io.projectreactor.kotlin:reactor-kotlin-extensions")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
-	implementation("org.springframework.cloud:spring-cloud-starter-zipkin")
 	implementation("io.github.microutils:kotlin-logging:1.8.3")
 	implementation("org.springframework.cloud:spring-cloud-starter-config")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
