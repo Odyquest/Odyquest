126f933e538eb9b0170db6f750d3dfc581071181
Johannes Bleier <johannes.bleier@gmx.de>
2020-08-16 14:13:07 +0200

    Generage chase component

---
+++ xaver-app/src/app/chase/chase.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-chase',
+  templateUrl: './chase.component.html',
+  styleUrls: ['./chase.component.scss']
+})
+export class ChaseComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
