a9a295b5567ef996f120a00cad094cd8d318c420
Johannes Bleier <johannes.bleier@gmx.de>
2020-08-17 20:08:11 +0200

    app: add component for home

--- xaver-app/src/app/app.module.ts
+++ xaver-app/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { ChaseComponent } from './chase/chase.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ChaseComponent,
+    HomeComponent,
   ],
   imports: [
     BrowserModule
