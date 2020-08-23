5384427ad60f777fd31de0190a34cb0503996ae8
Johannes Bleier <johannes.bleier@gmx.de>
2020-08-23 19:25:15 +0200

    app: add some chase elements

--- xaver-app/src/app/chase/chase.component.ts
+++ xaver-app/src/app/chase/chase.component.ts
@@ -1,11 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Chase } from '../chase';
+
 @Component({
   selector: 'app-chase',
   templateUrl: './chase.component.html',
   styleUrls: ['./chase.component.scss']
 })
 export class ChaseComponent implements OnInit {
+  chase: Chase
 
   constructor() { }
 
