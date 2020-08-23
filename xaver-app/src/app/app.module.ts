b243b88b73263097332152149ced927459f81989
Johannes Bleier <johannes.bleier@gmx.de>
2020-08-23 17:37:14 +0200

    app: update and add forms module

--- xaver-app/src/app/app.module.ts
+++ xaver-app/src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
@@ -14,7 +15,8 @@ import { HomeComponent } from './home/home.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
