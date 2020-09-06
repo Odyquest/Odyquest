162e0f562d9e8a6cc0afc61a6386acf5a4c687ef
Frederik Siepe <frederiksiepe@web.de>
2020-09-06 17:46:50 +0200

    now able to switch content based on dropdown

--- xaver-cms/src/app/quest-editor/quest-editor.component.ts
+++ xaver-cms/src/app/quest-editor/quest-editor.component.ts
@@ -7,9 +7,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class QuestEditorComponent implements OnInit {
 
+  hide_object_search = false
+  hide_input_term = true
+  hide_multiple_choice = true
+
   constructor() { }
 
   ngOnInit(): void {
+
+  }
+
+  onQuestTypeChange(value: String) {
+    console.log("Changed quest type to " + value);
+    switch (value) {
+      case "input-term":
+        this.hide_object_search = true
+        this.hide_input_term = false
+        this.hide_multiple_choice = true
+        break;
+      case "multiple-choice":
+        this.hide_object_search = true
+        this.hide_input_term = true
+        this.hide_multiple_choice = false
+        break;
+      case "object-search":
+        this.hide_object_search = false
+        this.hide_input_term = true
+        this.hide_multiple_choice = true
+        break;
+    }
   }
 
 }
