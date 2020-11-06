daac05362732a52eeeb5122522d0414e9b81bd84
Johannes Bleier <johannes.bleier@gmx.de>
2020-11-06 09:46:17 +0100

    App: handle submitting multiple choice quests

--- xaver-app/src/app/modules/components/submit-solution/submit-solution.component.ts
+++ xaver-app/src/app/modules/components/submit-solution/submit-solution.component.ts
@@ -5,6 +5,15 @@ import { Inject } from '@angular/core';
 
 import { Quest, QuestType } from '../../../shared/models/quest';
 
+class Item {
+  name: string;
+  selected: boolean;
+  constructor(name: string) {
+    this.name = name;
+    this.selected = false;
+  }
+}
+
 @Component({
   selector: 'app-submit-solution',
   templateUrl: './submit-solution.component.html',
@@ -13,10 +22,16 @@ import { Quest, QuestType } from '../../../shared/models/quest';
 export class SubmitSolutionComponent {
   quest = new Quest();
   textResult = '';
+  items = Array<Item>();
 
   constructor(public dialogRef: MatDialogRef<SubmitSolutionComponent>,
               @Inject(MAT_DIALOG_DATA) public data: any) {
     this.quest = this.data.quest;
+    if (this.isMultipleChoice()) {
+      for (const item of this.quest.requirementCombination.solutionItems) {
+        this.items.push(new Item(item));
+      }
+    }
   }
 
   submit(): void {
@@ -24,10 +39,14 @@ export class SubmitSolutionComponent {
     if (this.isText()) {
       result.push(this.textResult);
     } else if (this.isMultipleChoice()) {
-      // TODO collect data
-      console.log('TODO process submitted solution');
+      for (const item of this.items) {
+        if (item.selected) {
+          result.push(item.name);
+        }
+      }
+    } else {
+      // FIXME cover other cases
     }
-    // FIXME cover other cases
     this.dialogRef.close(result);
   }
 
