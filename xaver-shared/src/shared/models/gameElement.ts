70cd2119b50fc5386d6415b849074c6853f5a579
Johannes Bleier <johannes.bleier@gmx.de>
2020-12-05 16:56:21 +0100

    Add narrative type, external target and missing file

--- xaver-shared/src/shared/models/gameElement.ts
+++ xaver-shared/src/shared/models/gameElement.ts
@@ -2,9 +2,24 @@ import { Serializable, JsonProperty } from 'typescript-json-serializer';
 import { Description } from './description';
 
 @Serializable()
+export class ExecutionTarget {
+	@JsonProperty() url: string;
+	@JsonProperty() data: string;
+}
+
+/**
+ * Base class for views displayed by the app.
+ */
+@Serializable()
 export class GameElement {
 	@JsonProperty() id?: number;
 	@JsonProperty() version?: number;
 	@JsonProperty() title: string;
 	@JsonProperty() description: Description;
+
+	/** Url to external presenter of this element like Xaver
+	 * if variable is defined, the given presenter will be used
+	 * if it is undefined, use the app for display
+	 */
+  @JsonProperty() executionTarget: ExecutionTarget|undefined;
 }
--- xaver-shared/src/shared/models/narrative.ts
+++ xaver-shared/src/shared/models/narrative.ts
@@ -2,6 +2,11 @@ import { Serializable, JsonProperty } from 'typescript-json-serializer';
 import { GameElement } from './gameElement';
 import { XButton } from './xButton';
 
+export enum NarrativeType {
+  Text = 'text',
+  Panorama = 'panorama'
+}
+
 export enum NarrativeStatus {
   Continue = 'continue',
   Win = 'win',
@@ -13,6 +18,8 @@ export class Narrative extends GameElement {
 
 	/** Buttons for next steps if game continues */
 	@JsonProperty() buttons: Array<XButton>;
+	/** How to display the current narrative */
+	@JsonProperty() narrativeType = NarrativeType.Text;
 	/** Game continues of is finished */
 	@JsonProperty() narrativeStatus = NarrativeStatus.Continue;
 
