12e327f2bfe84dc16183d994f461786d5348117b
Frederik Siepe <frederik.siepe@robodev.eu>
2020-12-14 22:02:53 +0100

    shared data: adjusted another default value

--- xaver-shared/src/shared/models/description.ts
+++ xaver-shared/src/shared/models/description.ts
@@ -3,5 +3,5 @@ import {JsonProperty, Serializable} from 'typescript-json-serializer';
 @Serializable()
 export class Description {
   @JsonProperty('text') text = "";
-  @JsonProperty('image') image: string;
+  @JsonProperty('image') image = "";
 }
