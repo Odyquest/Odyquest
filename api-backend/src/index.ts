de97d8c493cd9bf5bc6f80423c63f344c9509ad7
Johannes Bleier <johannes.bleier@gmx.de>
2021-04-12 21:28:39 +0200

    api: add example cors headers

--- api-backend/src/index.ts
+++ api-backend/src/index.ts
@@ -1,6 +1,22 @@
 import express from 'express';
+import cors from 'cors';
 
 const app = express();
+
+const options: cors.CorsOptions = {
+  allowedHeaders: [
+    'Origin',
+    'X-Requested-With',
+    'Content-Type',
+    'Accept',
+    'X-Access-Token',
+  ],
+  credentials: true,
+  methods: 'GET,HEAD,OPTIONS,PUT,PATCH,POST,DELETE',
+  origin: 'localhost',
+  preflightContinue: false,
+};
+app.use(cors(options));
 
 app.get('/', (req, res) => {
     res.send('Boom!');
