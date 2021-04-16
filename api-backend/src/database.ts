ab783acfa40a5a9c2ebb53fc1fcfba32192fc653
Johannes Bleier <johannes.bleier@gmx.de>
2021-04-16 14:22:53 +0200

    api-backend: improve database.ts
    This commit is incomplete, in class ChaseMetaData the field id has to
    be renamed to chase_id

--- api-backend/src/database.ts
+++ api-backend/src/database.ts
@@ -1,6 +1,11 @@
-import { model, Schema } from 'mongoose';
+import { model, Document, Model, Schema } from 'mongoose';
 import { Chase, ChaseList, ChaseMetaData } from './shared/models/chase';
-import { Description } from './shared/models/description';
+//import { Description } from './shared/models/description';
+
+interface Description extends Document {
+  text: string;
+  image: string;
+}
 
 const DescriptionSchema = new Schema(
   {
@@ -12,12 +17,12 @@ const DescriptionSchema = new Schema(
 
 const ChaseMetaDataSchema = new Schema(
   {
-   id: {type: String, required: false },
+   chase_id: {type: String, required: true },
    version: {type: Number, required: false },
-   title: {type: String, required: false },
+   title: {type: String, required: true },
    description: {type: String, required: false },
    preview: {
-     description: DescriptionSchema
+     description: {type: DescriptionSchema }
    },
    author: {type: String, required: false },
    lastEdited: {type: Date, required: false },
@@ -36,8 +41,15 @@ const ChaseListSchema = new Schema(
   }
 );
 
-interface DescriptionDocument extends Description, Document {}
-interface ChaseMetaDataDocument extends ChaseMetaData, Document {}
-interface ChaseDocument extends Chase, Document {}
-interface ChaseListDocument extends ChaseList, Document {}
+interface DescriptionDocument extends Description, Document {
+  text: string|any;
+  image: string|any;
+  };
+interface ChaseMetaDataDocument extends ChaseMetaData, Document {};
+interface ChaseDocument extends Chase, Document {};
+interface ChaseListDocument extends ChaseList, Document {};
 
+const DescriptionModel: Model<DescriptionDocument> = model('Description', DescriptionSchema);
+//export const DescriptionModel = model('Description', DescriptionSchema);
+const ChaseMetaDataModel: Model<ChaseMetaDataDocument> = model('ChaseMetaData', ChaseMetaDataSchema);
+//export const ChaseMetaDataModel = model('ChaseMetaData', ChaseMetaDataSchema);
