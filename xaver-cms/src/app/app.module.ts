7031d51bb241dc180e6c3419da22540b2f7fd9c9
Johannes Bleier <johannes.bleier@robodev.eu>
2021-09-16 10:18:51 +0200

    Load auth configuration at runtime

--- xaver-cms/src/app/app.module.ts
+++ xaver-cms/src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-import { HomeComponent } from './components/home/home.component';
 import { HttpClientModule } from '@angular/common/http'
 import { MAT_DIALOG_DEFAULT_OPTIONS, MatDialogModule, MatDialogRef } from '@angular/material/dialog';
 import { MatAutocompleteModule } from '@angular/material/autocomplete';
@@ -43,6 +42,7 @@ import { AppComponent } from './app.component';
 import { AuthGuard } from './services/auth/auth.guard.service';
 import { ChaseSelectorComponent } from './components/chase-selector/chase-selector.component';
 import { CreateChaseDialogComponent } from './components/create-chase-dialog/create-chase-dialog.component';
+import { HomeComponent } from './components/home/home.component';
 import { LoggedOutComponent } from './components/logged-out/logged-out.component';
 import { LoginComponent } from './components/login/login.component';
 import { MainEditorComponent } from './components/main-editor/main-editor.component';
@@ -50,26 +50,31 @@ import { QuestEditorComponent } from './components/quest-editor/quest-editor.com
 import { RuntimeConfigurationService, runtimeInitializerFn } from './shared/services/runtime-configuration.service';
 import { SidebarComponent } from './components/ui-elements/sidebar/sidebar.component';
 
-// Could also go to its own file, but we just dump it next to the AppModule.
-const config: AuthConfig = {
-  issuer: 'https://cms.schnitzeljagd.landesmuseum.de/auth/realms/master',
-  clientId: 'test-cms',
-  redirectUri: window.location.origin + '/cms',
-  logoutUrl: 'WILL_BE_DONE_LATER',
-  silentRefreshRedirectUri: window.location.origin + '/silent-refresh.html',
-  scope: 'openid profile email',
-};
+import { environment } from '../environments/environment';
 
-// Could also go to its own file, but we just dump it next to the AppModule.
-const authModuleConfig: OAuthModuleConfig = {
-  // Inject "Authorization: Bearer ..." header for these APIs:
-  resourceServer: {
-    allowedUrls: ['http://localhost:8400/chase', 'http://localhost:8444/chase'],
-    sendAccessToken: true,
-  },
-};
+function getAuthConfig(): AuthConfig {
+  const config: AuthConfig = {
+    issuer: environment['issuer'],
+    clientId: environment['clientId'],
+    redirectUri: window.location.origin + '/cms',
+    logoutUrl: 'WILL_BE_DONE_LATER',
+    silentRefreshRedirectUri: window.location.origin + '/silent-refresh.html',
+    scope: 'openid profile email',
+  };
+  config.logoutUrl = `${config.issuer}v2/logout?client_id=${config.clientId}&returnTo=${encodeURIComponent(config.redirectUri)}`;
+  return config;
+}
 
-config.logoutUrl = `${config.issuer}v2/logout?client_id=${config.clientId}&returnTo=${encodeURIComponent(config.redirectUri)}`;
+function getAuthModuleConfig(): OAuthModuleConfig {
+  return {
+    // Inject "Authorization: Bearer ..." header for these APIs:
+    resourceServer: {
+      allowedUrls: [environment['allowedUrls']],
+      sendAccessToken: true,
+    },
+  };
+}
+
 
 // TODO: add resolver
 const appRoutes: Routes = [
@@ -150,10 +155,10 @@ const appRoutes: Routes = [
       deps: [RuntimeConfigurationService]
     },
     { provide: MatDialogRef, useValue: {} },
-    { provide: OAuthModuleConfig, useValue: authModuleConfig },
+    { provide: OAuthModuleConfig, useFactory: getAuthModuleConfig },
     { provide: ValidationHandler, useClass: JwksValidationHandler },
     { provide: OAuthStorage, useValue: localStorage },
-    { provide: AuthConfig, useValue: config },
+    { provide: AuthConfig, useFactory: getAuthConfig },
   ],
 
   bootstrap: [AppComponent]
